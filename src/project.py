666cffb68ef585441c75eb11c637f76d7eaf5522
Bao-Nhi Nguyen <baonhi2nguyen@gmail.com>
2026-05-07 05:11:56 -0500

    started pygame initialization

---
+++ src/project.py
@@ -0,0 +1,9 @@
+import pygame
+import random
+
+def main(): 
+    pygame.init()
+    pygame.display.set_caption("I Spy")
+    resolution = (1920, 1080) 
+    screen = pygame.display.set_mode(resolution)
+    
