4a4c598be7007ede9ce6475ea7031cf503d27f87
Bao-Nhi Nguyen <baonhi2nguyen@gmail.com>
2026-05-07 05:14:09 -0500

    Added escape option to close out of game

--- src/project.py
+++ src/project.py
@@ -6,4 +6,15 @@ def main():
     pygame.display.set_caption("I Spy")
     resolution = (1920, 1080) 
     screen = pygame.display.set_mode(resolution)
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            elif event.type == pygame.KEYDOWN: 
+                if event.key == pygame.K_ESCAPE:
+                    running = False
     
+
+if __name__ == "__main__": 
+    main()
