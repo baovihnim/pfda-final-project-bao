1941b81eefdc2707b1d13181db25207de9487783
Bao-Nhi Nguyen <baonhi2nguyen@gmail.com>
2026-05-10 11:20:23 -0500

    changed image resize to be ratio based to account for non-square images; need to prevent items from overlapping

--- src/project.py
+++ src/project.py
@@ -15,7 +15,8 @@ def item_placement(screen):
     for filename in os.listdir(folder):
         image = os.path.join(folder, filename)
         img = pygame.image.load(image).convert_alpha()
-        img_final = pygame.transform.scale(img, (100, 100))
+        divider = img.height // 175
+        img_final = pygame.transform.scale(img, ((img.width // divider), (img.height // divider)))
         x_coord = random.randrange(300, 1500)
         y_coord = random.randrange(200, 800)
         screen.blit(img_final, (x_coord, y_coord))
